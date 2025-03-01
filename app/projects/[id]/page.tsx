63a89b496c850bf7439da3dd23798d66bb15aa97
Ilham Pauzan <akunamazon4811@gmail.com>
2025-03-01 19:14:13 +0700

    params id

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,10 +1,13 @@
-"use client";
-
 import ProjectDetail from "@/components/ProjectDetail";
 
 type Props = {
   params: { id: string };
 };
+
+// Menentukan ID project secara statis (kalau nggak pakai API)
+export async function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }, { id: "4" }, { id: "5" }, { id: "6" }, { id: "7" }, { id: "8" }]; // Bisa ubah sesuai kebutuhan
+}
 
 export default function ProjectDetailPage({ params }: Props) {
   return <ProjectDetail projectId={params.id} />;
